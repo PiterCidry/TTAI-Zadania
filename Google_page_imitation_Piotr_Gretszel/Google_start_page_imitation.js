a883bdbb8b159890ac23946765e92d0e521c6098
PiterCidry <piotr.gretszel@gmail.com>
2019-04-07 17:55:47 +0200

    Top and bottom ready to go

--- Google_page_imitation_Piotr_Gretszel/Google_start_page_imitation.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    const luckyButton = document.getElementById("lucky-luke");
-
-    if (luckyButton) {
-        luckyButton.addEventListener("click", function () {
-            window.location.href = "https://www.google.com/doodles/";
-        });
-    }
-};
--- Google_page_imitation_Piotr_Gretszel/shared.js
+++ Google_page_imitation_Piotr_Gretszel/shared.js
@@ -1,143 +1,157 @@
-window.onload = function() {
-    const inputSearch = document.getElementById("search");
-    const linkToBeDeveloped = document.getElementsByClassName("linkToBeDeveloped");
+(function ($) {
+    window.onload = function () {
+        const inputSearch = document.getElementById("search");
+        const linkToBeDeveloped = document.getElementsByClassName("linkToBeDeveloped");
+        const luckyButton = document.getElementById("lucky-luke");
 
-    if (linkToBeDeveloped && linkToBeDeveloped.length > 0) {
-        for (let i = 0; i < linkToBeDeveloped.length; i++) {
-            linkToBeDeveloped[i].addEventListener("click",
-                function() {
-                    alert("Functionality to be developed later.");
+        if (luckyButton) {
+            luckyButton.addEventListener("click",
+                function () {
+                    window.location.href = "https://www.google.com/doodles/";
                 });
         }
-    }
 
-    window.$("#search").focus(function() {
-        window.$("#input-text").addClass("border-outline");
-    });
-
-    window.$("#search").focusout(function() {
-        if (!window.$("#input-text").is(":hover")) {
-            window.$("#input-text").removeClass("border-outline");
+        if (linkToBeDeveloped && linkToBeDeveloped.length > 0) {
+            for (let i = 0; i < linkToBeDeveloped.length; i++) {
+                linkToBeDeveloped[i].addEventListener("click",
+                    function () {
+                        alert("Functionality to be developed later.");
+                    });
+            }
         }
-    });
 
-    window.$("#input-text").hover(function() {
-        window.$(this).addClass("border-outline");
-    });
+        $("#search").focus(function () {
+            $("#input-text").addClass("border-outline");
+        });
 
-    window.$("#input-text").mouseleave(function() {
-        if (!window.$("#search").is(":focus")) {
-            window.$(this).removeClass("border-outline");
+        $("#search").focusout(function () {
+            if (!$("#input-text").is(":hover")) {
+                $("#input-text").removeClass("border-outline");
+            }
+        });
+
+        $("#input-text").hover(function () {
+            $(this).addClass("border-outline");
+        });
+
+        $("#input-text").mouseleave(function () {
+            if (!$("#search").is(":focus")) {
+                $(this).removeClass("border-outline");
+            }
+        });
+
+        if (inputSearch) {
+            inputSearch.focus();
         }
-    });
 
-    if (inputSearch) {
-        inputSearch.focus();
-    }
-};
-
-const Autocomplete = {
-    name: "autocomplete",
-    template: "#autocomplete",
-    props: {
-        items: {
-            type: Array,
-            required: false,
-            default: () => []
-        },
-
-        isAsync: {
-            type: Boolean,
-            required: false,
-            default: false
-        }
-    },
-
-    data() {
-        return {
-            isOpen: false,
-            results: [],
-            search: "",
-            isLoading: false,
-            arrowCounter: 0
+        window.goToMainPage = function () {
+            window.location.href = "Google_start_page_imitation.html";
         };
-    },
+    };
 
-    methods: {
-        onChange() {
-            this.$emit("input", this.search);
+    const autocomplete = {
+        name: "autocomplete",
+        template: "#autocomplete",
+        props: {
+            items: {
+                type: Array,
+                required: false,
+                default: () => []
+            },
 
-            if (this.isAsync) {
-                this.isLoading = true;
-            } else {
-                this.filterResults();
-                this.isOpen = true;
+            isAsync: {
+                type: Boolean,
+                required: false,
+                default: false
             }
+        },
 
-            if (this.search.length < 1 || this.results.length < 1) {
+        data() {
+            return {
+                isOpen: false,
+                results: [],
+                search: "",
+                isLoading: false,
+                arrowCounter: 0
+            };
+        },
+
+        methods: {
+            onChange() {
+                this.$emit("input", this.search);
+
+                if (this.isAsync) {
+                    this.isLoading = true;
+                } else {
+                    this.filterResults();
+                    this.isOpen = true;
+                }
+
+                if (this.search.length < 1 || this.results.length < 1) {
+                    this.isOpen = false;
+                }
+            },
+
+            filterResults() {
+                this.results = this.items.filter(item => {
+                    return item.toLowerCase().startsWith(this.search.toLowerCase());
+                });
+            },
+
+            setResult(result) {
+                this.search = result;
                 this.isOpen = false;
-            }
-        },
+            },
 
-        filterResults() {
-            this.results = this.items.filter(item => {
-                return item.toLowerCase().startsWith(this.search.toLowerCase());
-            });
-        },
+            onArrowDown() {
+                if (this.arrowCounter < this.results.length) {
+                    this.arrowCounter++;
+                }
+            },
 
-        setResult(result) {
-            this.search = result;
-            this.isOpen = false;
-        },
+            onArrowUp() {
+                if (this.arrowCounter > 0) {
+                    this.arrowCounter--;
+                }
+            },
 
-        onArrowDown() {
-            if (this.arrowCounter < this.results.length) {
-                this.arrowCounter++;
-            }
-        },
-
-        onArrowUp() {
-            if (this.arrowCounter > 0) {
-                this.arrowCounter--;
-            }
-        },
-
-        onEnter() {
-            this.search = this.results[this.arrowCounter];
-            this.isOpen = false;
-            this.arrowCounter = -1;
-        },
-
-        handleClickOutside(evt) {
-            if (!this.$el.contains(evt.target)) {
+            onEnter() {
+                this.search = this.results[this.arrowCounter];
                 this.isOpen = false;
                 this.arrowCounter = -1;
+            },
+
+            handleClickOutside(evt) {
+                if (!this.$el.contains(evt.target)) {
+                    this.isOpen = false;
+                    this.arrowCounter = -1;
+                }
             }
-        }
-    },
+        },
 
-    watch: {
-        items: function (val, oldValue) {
-            if (val.length !== oldValue.length) {
-                this.results = val;
-                this.isLoading = false;
+        watch: {
+            items: function (val, oldValue) {
+                if (val.length !== oldValue.length) {
+                    this.results = val;
+                    this.isLoading = false;
+                }
             }
+        },
+
+        mounted() {
+            document.addEventListener("click", this.handleClickOutside);
+        },
+
+        destroyed() {
+            document.removeEventListener("click", this.handleClickOutside);
         }
-    },
+    };
 
-    mounted() {
-        document.addEventListener("click", this.handleClickOutside);
-    },
-
-    destroyed() {
-        document.removeEventListener("click", this.handleClickOutside);
-    }
-};
-
-let vue = new Vue({
-    el: "#app",
-    name: "app",
-    components: {
-        autocomplete: Autocomplete
-    }
-});
+    const vue = new window.Vue({
+        el: "#app",
+        name: "app",
+        components: {
+            autocomplete: autocomplete
+        }
+    });
+}(jQuery));
