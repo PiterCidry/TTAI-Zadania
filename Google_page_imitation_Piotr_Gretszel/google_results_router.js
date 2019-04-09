85227ea4e60ae3738e672ac05391f50eed84d8f0
PiterCidry <piotr.gretszel@gmail.com>
2019-04-10 00:58:35 +0200

    Finished Vue.js routing

---
+++ Google_page_imitation_Piotr_Gretszel/google_results_router.js
@@ -0,0 +1,159 @@
+const Wszystko = {
+    template: "<ul>" +
+        "<li class='links-results links-results-big linkToBeDeveloped'>Scacciarla sospettoso trascinato uno cambieremo ape incontrato era cancellato</li>" +
+        "<li>https://www.sconvoltoriconosco.com/pl-pl/<span class='linkToBeDeveloped'>&#9662;</span></li>" +
+        "<li>Incontrato pensieroso ti tu la ingannaste. Osi intere riluce lancio rimase sfonda hai dal. Mia scarabei chiedete che duchessa verranno. Uscendo vacilla passato su lo ed divieto cintura. Veda re orti le viva ebro da su. Chiedete crediate orgoglio ora sul due sofferto. Moglie scorso un subito ad.</li>" +
+        "<li><span class='links-results links-results-small linkToBeDeveloped'>link1</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link2</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link3</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link4</span></li>" +
+        "</ul >"
+};
+
+const Wiadomosci = {
+    template: "<ul>" +
+        "<li class='links-results links-results-big linkToBeDeveloped'>Hello wiadomosci hello wiadomosci hello wiadomosci</li>" +
+        "<li>https://www.sconvoltoriconosco.com/pl-pl/<span class='linkToBeDeveloped'>&#9662;</span></li>" +
+        "<li>Incontrato pensieroso ti tu la ingannaste. Osi intere riluce lancio rimase sfonda hai dal. Mia scarabei chiedete che duchessa verranno. Uscendo vacilla passato su lo ed divieto cintura. Veda re orti le viva ebro da su. Chiedete crediate orgoglio ora sul due sofferto. Moglie scorso un subito ad.</li>" +
+        "<li><span class='links-results links-results-small linkToBeDeveloped'>link1</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link2</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link3</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link4</span></li>" +
+        "</ul >"
+};
+
+const Grafika = {
+    template: "<ul>" +
+        "<li class='links-results links-results-big linkToBeDeveloped'>Hello grafika hello grafika hello grafika</li>" +
+        "<li>https://www.sconvoltoriconosco.com/pl-pl/<span class='linkToBeDeveloped'>&#9662;</span></li>" +
+        "<li>Incontrato pensieroso ti tu la ingannaste. Osi intere riluce lancio rimase sfonda hai dal. Mia scarabei chiedete che duchessa verranno. Uscendo vacilla passato su lo ed divieto cintura. Veda re orti le viva ebro da su. Chiedete crediate orgoglio ora sul due sofferto. Moglie scorso un subito ad.</li>" +
+        "<li><span class='links-results links-results-small linkToBeDeveloped'>link1</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link2</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link3</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link4</span></li>" +
+        "</ul >"
+};
+
+const Zakupy = {
+    template: "<ul>" +
+        "<li class='links-results links-results-big linkToBeDeveloped'>Hello zakupy hello zakupy hello zakupy</li>" +
+        "<li>https://www.sconvoltoriconosco.com/pl-pl/<span class='linkToBeDeveloped'>&#9662;</span></li>" +
+        "<li>Incontrato pensieroso ti tu la ingannaste. Osi intere riluce lancio rimase sfonda hai dal. Mia scarabei chiedete che duchessa verranno. Uscendo vacilla passato su lo ed divieto cintura. Veda re orti le viva ebro da su. Chiedete crediate orgoglio ora sul due sofferto. Moglie scorso un subito ad.</li>" +
+        "<li><span class='links-results links-results-small linkToBeDeveloped'>link1</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link2</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link3</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link4</span></li>" +
+        "</ul >"
+};
+
+const Filmy = {
+    template: "<ul>" +
+        "<li class='links-results links-results-big linkToBeDeveloped'>Hello filmy filmy hello filmy</li>" +
+        "<li>https://www.sconvoltoriconosco.com/pl-pl/<span class='linkToBeDeveloped'>&#9662;</span></li>" +
+        "<li>Incontrato pensieroso ti tu la ingannaste. Osi intere riluce lancio rimase sfonda hai dal. Mia scarabei chiedete che duchessa verranno. Uscendo vacilla passato su lo ed divieto cintura. Veda re orti le viva ebro da su. Chiedete crediate orgoglio ora sul due sofferto. Moglie scorso un subito ad.</li>" +
+        "<li><span class='links-results links-results-small linkToBeDeveloped'>link1</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link2</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link3</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link4</span></li>" +
+        "</ul >"
+};
+
+const Wiecej = {
+    template: "<ul>" +
+        "<li class='links-results links-results-big linkToBeDeveloped'>Hello wiecej hello wiecej hello wiecej</li>" +
+        "<li>https://www.sconvoltoriconosco.com/pl-pl/<span class='linkToBeDeveloped'>&#9662;</span></li>" +
+        "<li>Incontrato pensieroso ti tu la ingannaste. Osi intere riluce lancio rimase sfonda hai dal. Mia scarabei chiedete che duchessa verranno. Uscendo vacilla passato su lo ed divieto cintura. Veda re orti le viva ebro da su. Chiedete crediate orgoglio ora sul due sofferto. Moglie scorso un subito ad.</li>" +
+        "<li><span class='links-results links-results-small linkToBeDeveloped'>link1</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link2</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link3</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link4</span></li>" +
+        "</ul >"
+};
+
+const Ustawienia = {
+    template: "<ul>" +
+        "<li class='links-results links-results-big linkToBeDeveloped'>Hello ustawienia hello ustawienia hello ustawienia</li>" +
+        "<li>https://www.sconvoltoriconosco.com/pl-pl/<span class='linkToBeDeveloped'>&#9662;</span></li>" +
+        "<li>Incontrato pensieroso ti tu la ingannaste. Osi intere riluce lancio rimase sfonda hai dal. Mia scarabei chiedete che duchessa verranno. Uscendo vacilla passato su lo ed divieto cintura. Veda re orti le viva ebro da su. Chiedete crediate orgoglio ora sul due sofferto. Moglie scorso un subito ad.</li>" +
+        "<li><span class='links-results links-results-small linkToBeDeveloped'>link1</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link2</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link3</span><span>&middot;</span><span class='links-results links-results-small linkToBeDeveloped'>link4</span></li>" +
+        "</ul >"
+};
+
+function changeActiveInactive(to, from) {
+    if (!from) {
+        document.getElementById(to).classList.remove("category-inactive");
+        document.getElementById(to).classList.add("category-active");
+    }
+    else if (to) {
+        document.getElementById(to).classList.remove("category-inactive");
+        document.getElementById(from).classList.remove("category-active");
+        document.getElementById(to).classList.add("category-active");
+        document.getElementById(from).classList.add("category-inactive");
+    }
+}
+
+const routes = [
+    {
+        path: "*",
+        redirect: "/wszystko",
+        component: Wszystko,
+        name: "default",
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    },
+    {
+        path: "/wszystko",
+        component: Wszystko,
+        name: "wszystko",
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    },
+    {
+        path: "/wiadomosci",
+        component: Wiadomosci,
+        name: "wiadomosci",
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    },
+    {
+        path: "/grafika",
+        component: Grafika,
+        name: "grafika",
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    },
+    {
+        path: "/zakupy",
+        component: Zakupy,
+        name: "zakupy",
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    },
+    {
+        path: "/filmy",
+        component: Filmy,
+        name: "filmy",
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    },
+    {
+        path: "/wiecej",
+        component: Wiecej,
+        name: "wiecej",
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    },
+    {
+        path: "/ustawienia",
+        name: "ustawienia",
+        component: Ustawienia,
+        beforeEnter: (to, from, next) => {
+            changeActiveInactive(to.name, from.name);
+            next();
+        }
+    }
+];
+
+const router = new VueRouter({
+    routes
+});
+
+const routerApp = new window.Vue({
+    router
+}).$mount("#routerApp");
